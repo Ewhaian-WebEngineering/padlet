8d608cffca219544edccca0883ff061b9fc1b8a3
sungeun <ksekse7147@gamil.com>
2025-09-11 01:19:20 +0900

    chore:유저 모델에서 익명 삭제

--- back/models/user.model.js
+++ back/models/user.model.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: { type: String , required: true},
     email: { type: String, required: true, unique: true },
-    anoymity: {type: Boolean, required: true} //익명여부
 })
 
 const User = mongoose.model("User", userSchema);
