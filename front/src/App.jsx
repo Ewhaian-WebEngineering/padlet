97ba19f45d120f2322a200ad7518be4755c26671
jiyu <jiyu06103@naver.com>
2025-09-14 21:39:07 +0900

    feat(#1): 로그인페이지 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QnaPage from "./pages/qnaPage/QnaPage";
+import LoginPage from "./pages/loginPage/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/qna" element={<QnaPage />} />
+        <Route path="/login" element={<LoginPage />} /> 
       </Routes>
     </Router>
   );
