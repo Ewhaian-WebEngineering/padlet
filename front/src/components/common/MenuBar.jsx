cb2a11eb33c23a54c8ad61e3bffe8edd49940250
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-09-29 16:39:25 +0900

    Feat: MenuBar naviage 추가

--- front/src/components/common/MenuBar.jsx
+++ front/src/components/common/MenuBar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import * as S from "./MenuBar.style";
+import { useNavigate } from "react-router-dom";
 
 export default function MenuBar({ pickMenu }) {
+  const navigate = useNavigate();
   return <S.Container>
-    <S.MenuButton $isPick={pickMenu == 0}>행사소개</S.MenuButton>
-    <S.MenuButton $isPick={pickMenu == 1}>QNA</S.MenuButton>
+    <S.MenuButton $isPick={pickMenu == 0} onClick={()=>{navigate("/event-info")}}>행사소개</S.MenuButton>
+    <S.MenuButton $isPick={pickMenu == 1} onClick={()=>{navigate("/qna")}}>QNA</S.MenuButton>
   </S.Container>;
 }
