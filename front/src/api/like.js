49e7613112db24e3fc8f1069cadc0aa35bb66b2f
bbandm <134187969+bbandm@users.noreply.github.com>
2025-10-02 12:14:29 +0900

    feat(#39): 좋아요 추가 삭제 프론트 재연동

--- front/src/api/like.js
+++ front/src/api/like.js
@@ -1,25 +1,5 @@
 import axiosInstance from "./axiosInstance";
 
-export const fetchQuestionById=async(id)=>{
-    try{
-        const res=await axiosInstance.get(`/question/detail/${id}`);
-        return res.data.question;
-    }catch(error){
-        console.error("질문 불러오기 실패:", error);
-        throw error;
-    }
-}
-
-export const deleteQuestion = async (id) => {
-    try {
-        const res = await axiosInstance.delete(`/question/${id}`);
-        return res.data.id; //id가 넘어옴
-    } catch (error) {
-        console.error("질문 삭제 api 실패:", error);
-        throw error;
-    }
-}
-
 export const postLike = async (questionId) => {
     try {
         const res = await axiosInstance.post("/like", { questionId });
